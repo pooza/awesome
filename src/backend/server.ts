1491fdb45bec6454151725683f1cf4bd4ffb3157
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-08-24 17:37:53 +0900

    .

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -5,7 +5,8 @@ import 'dotenv/config'
 
 const app = express()
 app.use(router)
-app.use(express.static(path.join(__dirname, '../public')))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 export default app.listen(process.env.PORT, () => {
   console.log(`http://localhost:${process.env.PORT}`)
