78082e41a9295197a02b7365862393871c1f164f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-08-24 18:49:34 +0900

    .

--- src/backend/router.ts
+++ src/backend/router.ts
@@ -3,7 +3,15 @@ import express from 'express'
 const router: express.Router = express.Router()
 
 router.get('/api/version', (req: express.Request, res: express.Response) => {
-  res.json({version: '0.0.1'})
+  res.json({
+    version: '0.0.1',
+  })
+})
+
+router.get('/api/greeting', (req: express.Request, res: express.Response) => {
+  res.json({
+    greeting: 'hello',
+  })
 })
 
 export default router
